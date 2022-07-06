175d4278f41984e7245c4702ebefa8153da0d2e6
Rosie Baish <rosie.baish@192-168.co.uk>
2022-07-06 19:36:20 +0100

    chore: Refactor out rustfmt stuff into separate file

--- build/main.rs
+++ build/main.rs
@@ -1,15 +1,13 @@
-use std::borrow::Cow;
 use std::env;
 use std::fs::File;
-use std::io;
 use std::io::prelude::*;
-use std::io::{BufWriter, Write};
-use std::path::{Path, PathBuf};
-use std::process::{Command, Stdio};
+use std::io::BufWriter;
+use std::path::Path;
 
 #[path = "../src/build_common.rs"]
 mod build_common;
 use build_common::*;
+mod rustfmt;
 
 #[derive(Debug)]
 struct Instruction {
@@ -140,90 +138,6 @@ fn parse_org_table(org_table: &str) -> Vec<Instruction> {
   instructions
 }
 
-/// Write these bindings as source text to the given `Write`able.
-pub fn write<W: std::io::Write>(
-  text: String,
-  writer: &mut BufWriter<W>,
-) -> io::Result<()> {
-  match rustfmt_generated_string(&text) {
-    Ok(formatted_text) => {
-      writer.write(formatted_text.as_bytes())?;
-    }
-    Err(err) => {
-      eprintln!("Failed to run rustfmt: {} (non-fatal, continuing)", err);
-      writer.write(text.as_bytes())?;
-    }
-  }
-  Ok(())
-}
-
-/// Gets the rustfmt path to rustfmt the generated bindings.
-fn rustfmt_path<'a>() -> io::Result<Cow<'a, PathBuf>> {
-  if let Ok(rustfmt) = env::var("RUSTFMT") {
-    return Ok(Cow::Owned(rustfmt.into()));
-  }
-  #[cfg(feature = "which-rustfmt")]
-  match which::which("rustfmt") {
-    Ok(p) => Ok(Cow::Owned(p)),
-    Err(e) => Err(io::Error::new(io::ErrorKind::Other, format!("{}", e))),
-  }
-  #[cfg(not(feature = "which-rustfmt"))]
-  Err(io::Error::new(
-    io::ErrorKind::Other,
-    "which wasn't enabled, and no rustfmt binary specified",
-  ))
-}
-
-/// Checks if rustfmt_bindings is set and runs rustfmt on the string
-fn rustfmt_generated_string<'a>(source: &'a str) -> io::Result<Cow<'a, str>> {
-  let rustfmt = rustfmt_path()?;
-  let mut cmd = Command::new(&*rustfmt);
-
-  cmd.stdin(Stdio::piped()).stdout(Stdio::piped());
-
-  let mut child = cmd.spawn()?;
-  let mut child_stdin = child.stdin.take().unwrap();
-  let mut child_stdout = child.stdout.take().unwrap();
-
-  let source = source.to_owned();
-
-  // Write to stdin in a new thread, so that we can read from stdout on this
-  // thread. This keeps the child from blocking on writing to its stdout which
-  // might block us from writing to its stdin.
-  let stdin_handle = ::std::thread::spawn(move || {
-    let _ = child_stdin.write_all(source.as_bytes());
-    source
-  });
-
-  let mut output = vec![];
-  io::copy(&mut child_stdout, &mut output)?;
-
-  let status = child.wait()?;
-  let source = stdin_handle.join().expect(
-    "The thread writing to rustfmt's stdin doesn't do \
-     anything that could panic",
-  );
-
-  match String::from_utf8(output) {
-    Ok(bindings) => match status.code() {
-      Some(0) => Ok(Cow::Owned(bindings)),
-      Some(2) => Err(io::Error::new(
-        io::ErrorKind::Other,
-        "Rustfmt parsing errors.".to_string(),
-      )),
-      Some(3) => {
-        println!("Rustfmt could not format some lines.");
-        Ok(Cow::Owned(bindings))
-      }
-      _ => Err(io::Error::new(
-        io::ErrorKind::Other,
-        "Internal rustfmt error".to_string(),
-      )),
-    },
-    _ => Ok(Cow::Owned(source)),
-  }
-}
-
 fn main() -> std::io::Result<()> {
   let mut file = File::open("rv64_i.org")?;
   let mut contents = String::new();
@@ -235,16 +149,16 @@ fn main() -> std::io::Result<()> {
 
   let mut instruction_map = phf_codegen::Map::new();
   for instruction in instructions {
-    write(instruction.create_implementation_source(), &mut file).unwrap();
+    rustfmt::write(instruction.create_implementation_source(), &mut file)
+      .unwrap();
     instruction_map
       .entry(instruction.mnemonic.clone(), &instruction.as_source());
   }
-  write(
-    format!("#[allow(unused_must_use)]\nstatic INSTRUCTIONS: phf::Map<&'static str, InstructionSource> = {}",
+  rustfmt::write(
+    format!("#[allow(unused_must_use)]\nstatic INSTRUCTIONS: phf::Map<&'static str, InstructionSource> = {};\n",
     instruction_map.build()),
     &mut file,
   )
   .unwrap();
-  write!(&mut file, ";\n").unwrap();
   Ok(())
 }
---
+++ build/rustfmt.rs
@@ -0,0 +1,90 @@
+use std::borrow::Cow;
+use std::env;
+use std::io;
+use std::io::*;
+use std::path::PathBuf;
+use std::process::{Command, Stdio};
+
+/// Write these bindings as source text to the given `Write`able.
+pub fn write<W: std::io::Write>(
+  text: String,
+  writer: &mut BufWriter<W>,
+) -> io::Result<()> {
+  match rustfmt_generated_string(&text) {
+    Ok(formatted_text) => {
+      writer.write(formatted_text.as_bytes())?;
+    }
+    Err(err) => {
+      eprintln!("Failed to run rustfmt: {} (non-fatal, continuing)", err);
+      writer.write(text.as_bytes())?;
+    }
+  }
+  Ok(())
+}
+
+/// Gets the rustfmt path to rustfmt the generated bindings.
+fn rustfmt_path<'a>() -> io::Result<Cow<'a, PathBuf>> {
+  if let Ok(rustfmt) = env::var("RUSTFMT") {
+    return Ok(Cow::Owned(rustfmt.into()));
+  }
+  #[cfg(feature = "which-rustfmt")]
+  match which::which("rustfmt") {
+    Ok(p) => Ok(Cow::Owned(p)),
+    Err(e) => Err(io::Error::new(io::ErrorKind::Other, format!("{}", e))),
+  }
+  #[cfg(not(feature = "which-rustfmt"))]
+  Err(io::Error::new(
+    io::ErrorKind::Other,
+    "which wasn't enabled, and no rustfmt binary specified",
+  ))
+}
+
+/// Checks if rustfmt_bindings is set and runs rustfmt on the string
+fn rustfmt_generated_string<'a>(source: &'a str) -> io::Result<Cow<'a, str>> {
+  let rustfmt = rustfmt_path()?;
+  let mut cmd = Command::new(&*rustfmt);
+
+  cmd.stdin(Stdio::piped()).stdout(Stdio::piped());
+
+  let mut child = cmd.spawn()?;
+  let mut child_stdin = child.stdin.take().unwrap();
+  let mut child_stdout = child.stdout.take().unwrap();
+
+  let source = source.to_owned();
+
+  // Write to stdin in a new thread, so that we can read from stdout on this
+  // thread. This keeps the child from blocking on writing to its stdout which
+  // might block us from writing to its stdin.
+  let stdin_handle = ::std::thread::spawn(move || {
+    let _ = child_stdin.write_all(source.as_bytes());
+    source
+  });
+
+  let mut output = vec![];
+  io::copy(&mut child_stdout, &mut output)?;
+
+  let status = child.wait()?;
+  let source = stdin_handle.join().expect(
+    "The thread writing to rustfmt's stdin doesn't do \
+     anything that could panic",
+  );
+
+  match String::from_utf8(output) {
+    Ok(bindings) => match status.code() {
+      Some(0) => Ok(Cow::Owned(bindings)),
+      Some(2) => Err(io::Error::new(
+        io::ErrorKind::Other,
+        "Rustfmt parsing errors.".to_string(),
+      )),
+      Some(3) => {
+        println!("Rustfmt could not format some lines.");
+        Ok(Cow::Owned(bindings))
+      }
+      _ => Err(io::Error::new(
+        io::ErrorKind::Other,
+        "Internal rustfmt error".to_string(),
+      )),
+    },
+    _ => Ok(Cow::Owned(source)),
+  }
+}
