047ef9c7f42f1e3948b8fd983f94979479a7e160
Rosie Baish <rosie.baish@192-168.co.uk>
2022-07-21 02:27:52 +0100

    refactor: Create an InterpreterTrait to allow some abstraction
    Refactor all the elements of Interpreter to not be public, and add a
    trait with the required interface.
    Rust doesn't have full inheritance, and we don't want the frontend to
    have to know/care about backend types, so all data to and from the UI
    is passed as strings and parsed in the backend.
    
    This doesn't yet allow multiple backends but it's a step in the right direction

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -2,19 +2,165 @@ use std::convert::TryInto;
 
 use crate::codegen::INSTRUCTIONS;
 use crate::instruction::*;
+use crate::rv64_i::MEMORY_SIZE;
+use crate::PC;
+
+pub trait InterpreterTrait {
+  fn create(initial_registers: Vec<String>) -> Self;
+  fn memory_size(&self) -> u32;
+  fn set_code(&mut self, code: String);
+  fn running(&self) -> bool;
+  fn set_running(&mut self, running: bool);
+  fn errors(&self) -> &Vec<String>;
+  fn warnings(&self) -> &Vec<String>;
+  fn registers_repr(&self) -> Vec<(String, String, String)>;
+  fn memory_byte_repr(&self, start: usize, len: usize) -> Vec<String>;
+  fn memory_ascii_repr(&self, start: usize, len: usize) -> Vec<String>;
+  fn toggle_breakpoint(&mut self, line_num: u32);
+  fn breakpoints(&self) -> Vec<bool>;
+  fn set_frequency(&mut self, frequency: Option<u32>);
+  fn next_inst_line_num(&self) -> u32;
+}
 
 pub struct Interpreter {
-  pub code: String,
-  pub instructions: Vec<Instruction>,
-  pub registers: [Register; 32],
-  pub memory: [u8; crate::rv64_i::MEMORY_SIZE],
-  pub pc: PC,
-  pub errors: Vec<String>,
-  pub warnings: Vec<String>,
+  code: String,
+  instructions: Vec<Instruction>,
+  registers: [Register; 32],
+  memory: [u8; crate::rv64_i::MEMORY_SIZE],
+  pc: PC,
+  errors: Vec<String>,
+  warnings: Vec<String>,
   // Some(0) means single step
   // None means "as fast as possible"
-  pub frequency: Option<u32>,
-  pub running: bool,
+  frequency: Option<u32>,
+  running: bool,
+}
+
+impl InterpreterTrait for Interpreter {
+  fn create(initial_registers: Vec<String>) -> Interpreter {
+    let mut interpreter = Interpreter {
+      code: "".to_string(),
+      instructions: Vec::new(),
+      registers: [Register { value: 0 }; 32],
+      memory: [0; MEMORY_SIZE],
+      pc: PC::new(),
+      errors: Vec::new(),
+      warnings: Vec::new(),
+      frequency: Some(0),
+      running: false,
+    };
+
+    for (i, r) in initial_registers.iter().enumerate() {
+      interpreter.registers[i + 1/* Skip 0 register because it's fixed*/] =
+        Register {
+          value: parse_int::parse::<u64>(r).expect("Successful conversion"),
+        };
+    }
+    interpreter
+  }
+
+  fn memory_size(&self) -> u32 {
+    MEMORY_SIZE as u32
+  }
+
+  fn set_code(&mut self, code: String) {
+    if code.ne(&self.code) {
+      self.code = code;
+      self.parse();
+    }
+  }
+
+  fn running(&self) -> bool {
+    self.running
+  }
+
+  fn set_running(&mut self, running: bool) {
+    self.running = running;
+  }
+
+  fn errors(&self) -> &Vec<String> {
+    &self.errors
+  }
+
+  fn warnings(&self) -> &Vec<String> {
+    &self.warnings
+  }
+
+  fn registers_repr(&self) -> Vec<(String, String, String)> {
+    let mut representations: Vec<(String, String, String)> = Vec::new();
+    for register in self.registers {
+      let repr = (
+        format!("{}", register),
+        format!("0x{:016X}", register),
+        format!("0b{:064b}", register),
+      );
+      representations.push(repr);
+    }
+    representations
+  }
+
+  fn memory_byte_repr(&self, start: usize, len: usize) -> Vec<String> {
+    let mut strings: Vec<String> = Vec::new();
+    for b in start..start + len {
+      strings.push(format!("{:02x}", self.memory[b as usize]));
+    }
+    strings
+  }
+
+  fn memory_ascii_repr(&self, start: usize, len: usize) -> Vec<String> {
+    vec![self.memory[start..(start + len)]
+      .iter()
+      .map(|num| {
+        if *num >= 32 && *num <= 126 {
+          (*num as char).to_string()
+        } else {
+          '.'.to_string()
+        }
+      })
+      .collect()]
+  }
+
+  fn toggle_breakpoint(&mut self, line_num: u32) {
+    for mut instruction in self.instructions.iter_mut() {
+      if instruction.line_num == line_num {
+        instruction.breakpoint = !instruction.breakpoint;
+        log!("{:?}", instruction);
+      }
+    }
+  }
+
+  fn breakpoints(&self) -> Vec<bool> {
+    let max_line_num = self
+      .instructions
+      .iter()
+      .map(|i| i.line_num)
+      .max()
+      .unwrap_or(0) as usize;
+
+    // Have to create and then set, because of blank lines
+    let mut is_break: Vec<bool> = Vec::with_capacity(max_line_num);
+    for _i in 0..max_line_num {
+      is_break.push(false);
+    }
+    for instruction in &self.instructions {
+      is_break[(instruction.line_num - 1/* 1 indexed */) as usize] =
+        instruction.breakpoint;
+    }
+    is_break
+  }
+
+  fn set_frequency(&mut self, frequency: Option<u32>) {
+    self.frequency = frequency;
+  }
+
+  fn next_inst_line_num(&self) -> u32 {
+    let next_inst_num = (self.pc.get().value / 4) as usize;
+    if next_inst_num < self.instructions.len() {
+      self.instructions[next_inst_num].line_num - 1 /* 1 indexed */
+    } else {
+      0
+    }
+  }
 }
 
 impl Interpreter {
